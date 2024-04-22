80febd832e8ae1c78a25122c987402bd049086d3
crin706 <rosh-ka@mail.ru>
2024-04-22 23:38:19 +0300

    Работа с массивами и весами

--- HW8/Task1/src/Main.java
+++ HW8/Task1/src/Main.java
@@ -1,15 +1,29 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         //#1 Создать массив из 10 целых чисел. Значения могут быть любыми
-        //todo
-
         //#2 Распечатать все значения массива начиная с 0 до последнего индексов.
-        //todo
+
+        int nums[] = new int[10];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = new Random().nextInt(100);
+            System.out.print(nums[i] + " ");
+        }
+        System.out.println();
 
         //#3 Распечатать все значения массива с конца (начиная с последнего до 0 индексов).
-        //todo
-
+        int index = 0;
+        int reverseNums[] = new int[10];
+        for (int i = nums.length - 1; i >= 0; i--) {
+            System.out.print(nums[i] + " ");
+            reverseNums[index] = nums[i];
+            index++;
+        }
+        System.out.println();
         //#4 *** Задание особой сложности. Развернуть массив(см конец урока).
-        //todo
+        for (int i = 0; i < reverseNums.length; i++) {
+            System.out.print(reverseNums[i] + " ");
+        }
     }
 }
--- HW8/Task2/src/Main.java
+++ HW8/Task2/src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        //todo код программы писать тут
+        int nums[] = new int[10];
+        int sumOfWeights = 0;
+        int specialCondition = 0;
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = new Random().nextInt(40, 100);
+            System.out.print(nums[i] + " ");
+            sumOfWeights += nums[i];
+            if (nums[i] >= 60 && nums[i] <= 80) {
+                specialCondition++;
+            }
+        }
+        double middleWeight = sumOfWeights / 10.0;
+        System.out.println();
+        System.out.println("Средний вес: " + middleWeight);
+        System.out.printf("У %d людей вес находится в промежутке от 60 до 80 кг включительно.", specialCondition);
     }
 }
